4ad26e1f7b9f9b6d82541a0fad624c37885260fd
Harrison Wallin <harrisonwallin@gmail.com>
2024-02-04 17:09:35 -0800

    update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fairwayscapital.com',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt()]
-});
+  site: 'https://www.fairwayscapital.com',
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt()],
+})
